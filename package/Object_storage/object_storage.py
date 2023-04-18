6570ddfe1c7508824733ea090169141b3aebac23
Turist412 <alex.tepl03@gmail.com>
2023-04-18 21:58:11 +0300

    msg

---
+++ package/Object_storage/object_storage.py
@@ -0,0 +1,66 @@
+from package.object_manager.manager import ObjectManager
+
+class ObjectStorage:
+    def __init__(self):
+        self.objManager = ObjectManager()
+        self.storage = dict()
+        self.SWO = SWO()
+        self.PW = PW()
+        self.counter = 0
+
+    def delete(self, uid):
+        del self.storage[uid]
+
+    def update(self,uid, new_data:dict):
+        self.storage[uid] = new_data
+
+    def create(self, to_create:dict):
+        if to_create["name"] == '':
+            to_create["name"] = 'untitled_' + str(self.counter)
+            self.counter += 1
+
+        if to_create["FigureTypes"] == "Cone":
+            uid = self.objManager.create_cone(to_create["curve"],
+                                        to_create["point"],
+                                        to_create["t_bounce"],
+                                        to_create["v_bounce"])
+            self.storage[uid] = to_create
+            self.SWO.Display(uid,to_create["name"])
+            self.PW.add_mesh(uid,self.objManager.get_figure_mesh(uid),**self.objManager.get_figure_settings(uid))
+        elif to_create["FigureTypes"] == "Cylinder":
+            uid = self.objManager.create_cylinder(to_create["curve"],
+                                        to_create["direction"],
+                                        to_create["t_bounce"],
+                                        to_create["v_bounce"])
+            self.storage[uid] = to_create
+            self.SWO.Display(uid, to_create["name"])
+            self.PW.add_mesh(uid,self.objManager.get_figure_mesh(uid),**self.objManager.get_figure_settings(uid))
+        elif to_create["FigureTypes"] == "Curve":
+            uid = self.objManager.create_curve(to_create["curve"],
+                                        to_create["t_bounce"])
+            self.storage[uid] = to_create
+            self.SWO.Display(uid, to_create["name"])
+            self.PW.add_mesh(uid,self.objManager.get_figure_mesh(uid),**self.objManager.get_figure_settings(uid))
+        elif to_create["FigureTypes"] == "Line":
+            uid = self.objManager.create_line(to_create["point1"],
+                                        to_create["point2"],
+                                        to_create["t_bounce"])
+            self.storage[uid] = to_create
+            self.SWO.Display(uid, to_create["name"])
+            self.PW.add_mesh(uid,self.objManager.get_figure_mesh(uid),**self.objManager.get_figure_settings(uid))
+        elif to_create["FigureTypes"] == "Plane":
+            uid = self.objManager.create_plane(to_create["normal"],
+                                        to_create["point"],
+                                        to_create["size"])
+            self.storage[uid] = to_create
+            self.SWO.Display(uid, to_create["name"])
+            self.PW.add_mesh(uid,self.objManager.get_figure_mesh(uid),**self.objManager.get_figure_settings(uid))
+        elif to_create["FigureTypes"] == "Revolution":
+            uid = self.objManager.create_revolution_surface(to_create["curve"],
+                                        to_create["direction"],
+                                        to_create["t_bounce"])
+            self.storage[uid] = to_create
+            self.SWO.Display(uid, to_create["name"])
+            self.PW.add_mesh(uid,self.objManager.get_figure_mesh(uid),**self.objManager.get_figure_settings(uid))
+        else:
+            raise Exception("Invalid Figure type")
