ff7e55b31bffe847dac01b5a3d3922d77ec44e05
HellAbyss <jeremykovalchuck@gmail.com>
2023-04-23 22:11:17 +0300

    Refactor and fixed bugs

---
+++ package/ui/gui.py
@@ -0,0 +1,425 @@
+from PyQt5.QtWidgets import *
+import pyvista as pv
+from pyvistaqt import QtInteractor
+from package.ui.Widgets.CustomWidgets import Creator
+import package.ui.Widgets.generated.userinterface_ as userinterface_
+
+import ast
+
+def parse_expression(expr_str):
+    # Define a list of allowed names
+    allowed_names = {'t'}
+
+    # Parse the expression into an abstract syntax tree
+    expr_ast = ast.parse(expr_str, mode='eval')
+
+    # Check if the expression is a valid mathematical expression with only 't' variable
+    for node in ast.walk(expr_ast):
+        if isinstance(node, ast.Name) and node.id not in allowed_names:
+            raise ValueError(f"Expression contains an invalid variable '{node.id}'. Only 't' is allowed.")
+
+    # Return the parsed expression
+    return ast.Expression(expr_ast.body)
+
+#                                                    #
+# REPLACE THIS FUNCTION WITH THE ONE FROM @fenik_fam #
+#                       vvv                          #
+
+class PyvistaPyQtWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # Create the QtInteractor object
+        self.interactor = QtInteractor(self)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.interactor)
+        self.setLayout(self.layout)
+
+        # Initialize the Pyvista plotter
+        self.plotter = self.interactor
+
+        self.plotter.enable_depth_peeling(10)
+
+
+
+    def add_mesh(self, mesh, **kwargs):
+        self.plotter.add_mesh(mesh, **kwargs)
+        self.plotter.reset_camera()
+        self.plotter.update()
+
+    def test_scene(self):
+        sphere = pv.Sphere()
+        self.add_mesh(sphere, opacity=0.50, color="red")
+        self.add_mesh(pv.Sphere(2, (1, 1, 1)), opacity=0.5, color="red")
+
+        self.add_mesh(pv.Sphere(2, (3, 1, 1)), opacity=0.50, color="red")
+        self.add_mesh(pv.Sphere(0.5, (3, -0.7, 1)), opacity=0.5, color="red")
+
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+        # Load ui file
+        #uic.loadUi("ui/userinterface.ui",self)
+
+        _ui = userinterface_.Ui_MainWindow()
+        _ui.setupUi(self)
+        _ui.retranslateUi(self)
+
+        # Define our widgets
+        self.toolbox = self.findChild(QGroupBox, 'tools_box')
+        self.toolbox_layout = QVBoxLayout()
+        self.inside = QWidget()
+        self.toolbox_layout.addWidget(self.inside)
+        self.toolbox.setLayout(self.toolbox_layout)
+
+        self.widget = self.findChild(QWidget, 'widget')
+        self.scrollArea = self.findChild(QScrollArea, 'scrollArea')
+        self.console = self.findChild(QTextBrowser, 'textBrowser')
+
+        self.save = self.findChild(QAction, 'save')
+        self.save_as = self.findChild(QAction, 'save_as')
+        self.save_image = self.findChild(QAction, 'save_image')
+        self.open = self.findChild(QAction, 'open')
+        self.new_scene = self.findChild(QAction, 'new_scene')
+        self.show_tools = self.findChild(QAction, 'tools')
+        self.show_console = self.findChild(QAction, 'console')
+        self.setting = self.findChild(QAction, 'setting')
+
+        self.creator = Creator()
+
+
+
+        # Define layout for future replacement
+        self.main_layout = self.findChild(QHBoxLayout, 'horizontalLayout')
+
+        # Initializing pyvista scene
+        self.pyvista_widget = PyvistaPyQtWidget(self)
+
+
+        self.main_layout.removeWidget(self.widget)
+        self.main_layout.removeWidget(self.toolbox)
+
+        self.main_layout.addWidget(self.pyvista_widget, 1)
+        self.pyvista_widget.test_scene()
+
+        self.main_layout.addWidget(self.toolbox)
+
+        # Click the action
+        self.show_tools.triggered.connect(self.hide_unhide_tools)
+        self.show_console.triggered.connect(self.hide_unhide_console)
+
+        #self.new_scene.triggered.connect(self.add_object)
+
+        self.add_object()
+
+        # Show the app
+        self.show()
+
+        # Keep track of hidden or not
+        self.hidden_tools = False
+        self.hidden_console = False
+
+
+
+    def add_object(self): # DEBUG VERSION, SUBJECT TO CHANGE
+
+        #contextMenu = QMenu(self)
+
+        #menu_choice_conical = contextMenu.addAction("Add conical surface")
+        #menu_choice_conical.triggered.connect(lambda: self.openCreateWidget(0))
+
+
+
+        self.choiceWidget = QWidget()
+        choiceWidgetLayout = QVBoxLayout()
+
+        choice_conical = QPushButton("Add conical surface")
+        choice_conical.clicked.connect(lambda: self.openCreateWidget(0))
+        choiceWidgetLayout.addWidget(choice_conical)
+
+        choice_curve = QPushButton("Add curve")
+        choice_curve.clicked.connect(lambda: self.openCreateWidget(1))
+        choiceWidgetLayout.addWidget(choice_curve)
+
+        choice_cylindrical = QPushButton("Add cylindrical surface")
+        choice_cylindrical.clicked.connect(lambda: self.openCreateWidget(2))
+        choiceWidgetLayout.addWidget(choice_cylindrical)
+
+        choice_line = QPushButton("Add line")
+        choice_line.clicked.connect(lambda: self.openCreateWidget(3))
+        choiceWidgetLayout.addWidget(choice_line)
+
+        choice_plane = QPushButton("Add plane")
+        choice_plane.clicked.connect(lambda: self.openCreateWidget(4))
+        choiceWidgetLayout.addWidget(choice_plane)
+
+        choice_point = QPushButton("Add point")
+        choice_point.clicked.connect(lambda: self.openCreateWidget(5))
+        choiceWidgetLayout.addWidget(choice_point)
+
+        choice_rotation = QPushButton("Add rotation surface")
+        choice_rotation.clicked.connect(lambda: self.openCreateWidget(6))
+        choiceWidgetLayout.addWidget(choice_rotation)
+
+        choice_vector = QPushButton("Add vector")
+        choice_vector.clicked.connect(lambda: self.openCreateWidget(7))
+        choiceWidgetLayout.addWidget(choice_vector)
+
+        self.choiceWidget.setLayout(choiceWidgetLayout)
+
+        print("Showing win2")
+
+        self.toolbox_layout.removeWidget(self.inside)
+        self.inside.deleteLater()
+
+        self.inside = self.choiceWidget
+        self.toolbox_layout.addWidget(self.inside)
+        self.toolbox_layout.update()
+
+
+    def openCreateWidget(self, _id: int):
+
+        applyButton = None
+
+
+
+        match _id:
+            case 0:
+                self.createWidget = self.creator.CreateConicalWidget()
+            case 1:
+                self.createWidget = self.creator.CreateCurveWidget()
+            case 2:
+                self.createWidget = self.creator.CreateCylindricalWidget()
+            case 3:
+                self.createWidget = self.creator.CreateLineWidget()
+            case 4:
+                self.createWidget = self.creator.CreatePlaneWidget()
+            case 5:
+                self.createWidget = self.creator.CreatePointWidget()
+            case 6:
+                self.createWidget = self.creator.CreateRotationFigureWidget()
+            case 7:
+                self.createWidget = self.creator.CreateVectorWidget()
+            case _:
+                self.createWidget = QWidget()
+
+        self.toolbox_layout.removeWidget(self.inside)
+        self.inside.deleteLater()
+
+        self.inside = self.createWidget
+        self.toolbox_layout.addWidget(self.inside)
+        self.toolbox_layout.update()
+
+        print(_id)
+        self.createWidget.Form.applyButton.clicked.connect(lambda: createObject(_id))
+        self.createWidget.Form.cancelButton.clicked.connect(self.add_object)
+
+
+
+        def input_point():
+
+            point_input_x = self.findChild(QLineEdit, "point_input_x").text()
+            point_input_y = self.findChild(QLineEdit, "point_input_y").text()
+            point_input_z = self.findChild(QLineEdit, "point_input_z").text()
+            try:
+                point_input_x = float(point_input_x)
+                point_input_y = float(point_input_y)
+                point_input_z = float(point_input_z)
+            except:
+                self.console.append("Incorrect value in point input")
+                return False
+
+            return point_input_x, point_input_y, point_input_z
+
+        def input_vector():
+            vector_input_x = self.findChild(QLineEdit, "vector_input_x").text()
+            vector_input_y = self.findChild(QLineEdit, "vector_input_y").text()
+            vector_input_z = self.findChild(QLineEdit, "vector_input_z").text()
+            try:
+                vector_input_x = float(vector_input_x)
+                vector_input_y = float(vector_input_y)
+                vector_input_z = float(vector_input_z)
+            except:
+                self.console.append("Incorrect value in vector input")
+                return False
+    
+            return vector_input_x, vector_input_y, vector_input_z
+        
+        def input_curve():
+
+            curve_input_x = self.findChild(QLineEdit, "curve_input_x").text()
+            curve_input_y = self.findChild(QLineEdit, "curve_input_y").text()
+            curve_input_z = self.findChild(QLineEdit, "curve_input_z").text()
+
+            _curve_input_x_text = curve_input_x
+            _curve_input_y_text = curve_input_y
+            _curve_input_z_text = curve_input_z
+
+            try:
+                curve_input_x = parse_expression(curve_input_x)
+                curve_input_y = parse_expression(curve_input_y)
+                curve_input_z = parse_expression(curve_input_z)
+            except:
+                self.console.append("Incorrect value in curve input")
+                return False
+
+            return curve_input_x, curve_input_y, curve_input_z
+
+        def input_line():
+
+            point_input_x_1 = self.findChild(QLineEdit, "input_x_1").text()
+            point_input_y_1 = self.findChild(QLineEdit, "input_y_1").text()
+            point_input_z_1 = self.findChild(QLineEdit, "input_z_1").text()
+            try:
+                point_input_x_1 = float(point_input_x_1)
+                point_input_y_1 = float(point_input_y_1)
+                point_input_z_1 = float(point_input_z_1)
+            except:
+                self.console.append("Incorrect value in line`s point 1 input")
+                return False
+
+            point_input_x_2 = self.findChild(QLineEdit, "input_x_2").text()
+            point_input_y_2 = self.findChild(QLineEdit, "input_y_2").text()
+            point_input_z_2 = self.findChild(QLineEdit, "input_z_2").text()
+            try:
+                point_input_x_2 = float(point_input_x_2)
+                point_input_y_2 = float(point_input_y_2)
+                point_input_z_2 = float(point_input_z_2)
+            except:
+                self.console.append("Incorrect value in line`s point 2 input")
+                return False
+
+            return point_input_x_1, point_input_y_1, point_input_z_1, point_input_x_2, point_input_y_2, point_input_z_2
+
+        def createObject(id):
+
+            match id:
+                case 0:  # Conical surface
+
+                    if not input_point():
+                        return
+                    point_input_x, point_input_y, point_input_z = input_point()
+
+                    if not input_curve():
+                        return
+                    curve_input_x, curve_input_y, curve_input_z = input_curve()
+
+
+                    print("test")
+
+                    self.console.append(
+                        f"For this conical surface point x = {point_input_x}, y = {point_input_y}, z = {point_input_z}")
+                    self.console.append(
+                        f"For this conical surface curve x = {str(curve_input_x)}, y = {str(curve_input_y)}, z = {str(curve_input_z)}")
+
+
+                    self.draw_object_on_screen("Conical")
+
+                case 1:  # Curve surface
+
+                    if not input_curve():
+                        return
+                    curve_input_x, curve_input_y, curve_input_z = input_curve()
+
+                    self.console.append(
+                        f"For this conical surface curve x = {curve_input_x}, y = {curve_input_y}, z = {curve_input_z}")
+
+                    self.draw_object_on_screen("Curve")
+
+                case 2:  # Cylindrical surface
+
+                    if not input_vector():
+                        return
+                    vector_x, vector_y, vector_z = input_vector()
+
+                    if not input_curve():
+                        return
+                    curve_input_x, curve_input_y, curve_input_z = input_curve()
+
+                    self.console.append(
+                        f"For this Cylindrical surface vector x = {vector_x}, y = {vector_y}, z = {vector_z}")
+
+                    self.console.append(
+                        f"For this Cylindrical surface curve x = {str(curve_input_x)}, y = {str(curve_input_y)}, z = {str(curve_input_z)}")
+
+
+                case 3:  # Create line
+
+                    if not input_line():
+                        return
+                    line_x1, line_y1, line_z1, line_x2, line_y2, line_z2 = input_line()
+
+                    self.console.append(
+                        f"For this line x1 = {line_x1}, y1 = {line_y1}, z1 = {line_z1}"
+                        f"              x2 = {line_x2}, y2 = {line_y2}, z2 = {line_z2}")
+
+                case 4: # Create plane
+
+                    if not input_point():
+                        return
+                    point_input_x, point_input_y, point_input_z = input_point()
+
+                    if not input_vector():
+                        return
+                    vector_input_x, vector_input_y, vector_input_z = input_vector()
+
+                    self.console.append(
+                        f"For this Plane point x = {point_input_x}, y = {point_input_y}, z = {point_input_z}")
+                    self.console.append(
+                        f"For this Plane normal vector x = {vector_input_x}, y = {vector_input_y}, z = {vector_input_z}")
+
+                case 5:  # Create point
+                    if not input_point():
+                        return
+                    point_input_x, point_input_y, point_input_z = input_point()
+
+                    self.console.append(
+                        f"For this point x = {point_input_x}, y = {point_input_y}, z = {point_input_z}")
+
+                case 6:  # Create Rotation Surface
+
+                    if not input_line():
+                        return
+                    line_x1, line_y1, line_z1, line_x2, line_y2, line_z2 = input_line()
+
+                    if not input_curve():
+                        return
+                    curve_input_x, curve_input_y, curve_input_z = input_curve()
+
+                    self.console.append(
+                        f"For this Rotation surface line x1 = {line_x1}, y1 = {line_y1}, z1 = {line_z1}"
+                        f"              x2 = {line_x2}, y2 = {line_y2}, z2 = {line_z2}")
+
+                    self.console.append(
+                        f"For this Cylindrical surface curve x = {str(curve_input_x)}, y = {str(curve_input_y)}, z = {str(curve_input_z)}")
+
+                case 7: # Create vector
+
+                    if not input_vector():
+                        return
+                    vector_input_x, vector_input_y, vector_input_z = input_vector()
+                    self.console.append(
+                        f"For this  vector x = {vector_input_x}, y = {vector_input_y}, z = {vector_input_z}")
+
+
+
+    def draw_object_on_screen(self, *args):
+        self.console.append(f"Drawing {args[0]}")
+
+
+    def hide_unhide_tools(self):
+        if self.hidden_tools:
+            self.toolbox.show()
+            self.hidden_tools = False
+        else:
+            self.toolbox.hide()
+            self.hidden_tools = True
+
+    def hide_unhide_console(self):
+        if self.hidden_console:
+            self.console.show()
+            self.hidden_console = False
+        else:
+            self.console.hide()
+            self.hidden_console = True
+
