29dd0f91f8355704dfc44b1eb48bbe0c6b382319
HellAbyss <jeremykovalchuck@gmail.com>
2023-04-23 22:11:17 +0300

    Refactor and fixed bugs

---
+++ package/ui/Widgets/CustomWidgets.py
@@ -0,0 +1,103 @@
+"""
+    This file converts custom widgets` .ui files to QWidget classes.
+    Nothing should be changed here, unless new files are added
+
+    Import this file as:
+
+    from Widgets.CustomWidgets import *
+
+"""
+
+from PyQt5 import QtWidgets
+import package.ui.Widgets.generated.CreateConicalWidget_ as CreateConicalWidget_
+import package.ui.Widgets.generated.CreateCurveWidget_ as CreateCurveWidget_
+import package.ui.Widgets.generated.CreateRotationFigureWidget_ as CreateRotationFigureWidget_
+import package.ui.Widgets.generated.CreateLineWidget_ as CreateLineWidget_
+import package.ui.Widgets.generated.CreatePlaneWidget_ as CreatePlaneWidget_
+import package.ui.Widgets.generated.CreatePointWidget_ as CreatePointWidget_
+import package.ui.Widgets.generated.CreateCylindricalWidget_ as CreateCylindricalWidget_
+import package.ui.Widgets.generated.CreateVectorWidget_ as CreateVectorWidget_
+
+
+class Creator:
+
+    def __init__(self):
+        ...
+
+    def CreateConicalWidget(self):
+
+        Widget = QtWidgets.QWidget()
+        Form = CreateConicalWidget_.Ui_Form()
+        Form.setupUi(Widget)
+        Form.retranslateUi(Widget)
+        Widget.Form = Form
+
+        return Widget
+
+    def CreateCurveWidget(self):
+        Widget = QtWidgets.QWidget()
+        Form = CreateCurveWidget_.Ui_Form()
+        Form.setupUi(Widget)
+        Form.retranslateUi(Widget)
+        Widget.Form = Form
+
+        return Widget
+
+    def CreateCylindricalWidget(self):
+        Widget = QtWidgets.QWidget()
+        Form = CreateCylindricalWidget_.Ui_Form()
+        Form.setupUi(Widget)
+        Form.retranslateUi(Widget)
+        Widget.Form = Form
+
+        return Widget
+
+    def CreateLineWidget(self):
+        Widget = QtWidgets.QWidget()
+        Form = CreateLineWidget_.Ui_Form()
+        Form.setupUi(Widget)
+        Form.retranslateUi(Widget)
+        Widget.Form = Form
+
+        return Widget
+
+    def CreatePlaneWidget(self):
+
+        Widget = QtWidgets.QWidget()
+        Form = CreatePlaneWidget_.Ui_Form()
+        Form.setupUi(Widget)
+        Form.retranslateUi(Widget)
+        Widget.Form = Form
+
+        return Widget
+
+    def CreatePointWidget(self):
+
+        Widget = QtWidgets.QWidget()
+        Form = CreatePointWidget_.Ui_Form()
+        Form.setupUi(Widget)
+        Form.retranslateUi(Widget)
+        Widget.Form = Form
+
+        return Widget
+
+    def CreateRotationFigureWidget(self):
+
+        Widget = QtWidgets.QWidget()
+        Form = CreateRotationFigureWidget_.Ui_Form()
+        Form.setupUi(Widget)
+        Form.retranslateUi(Widget)
+        Widget.Form = Form
+
+        return Widget
+
+    def CreateVectorWidget(self):
+
+        Widget = QtWidgets.QWidget()
+        Form = CreateVectorWidget_.Ui_Form()
+        Form.setupUi(Widget)
+        Form.retranslateUi(Widget)
+        Widget.Form = Form
+
+        return Widget
+
