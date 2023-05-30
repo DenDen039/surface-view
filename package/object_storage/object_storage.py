aadb2f9749bd1cded0d70fef132dd9dcc95c8ba2
Turist412 <alex.tepl03@gmail.com>
2023-05-30 13:17:17 +0300

    to surface

--- package/object_storage/object_storage.py
+++ package/object_storage/object_storage.py
@@ -180,9 +180,9 @@ class ObjectStorage:
 
             new_data["curve_string"] = new_data["curve"]
 
-            new_data["curve"] = [self.parser.parse_expression_string_to_lambda(new_data["curve"][0]),
-                                 self.parser.parse_expression_string_to_lambda(new_data["curve"][1]),
-                                 self.parser.parse_expression_string_to_lambda(new_data["curve"][2])]
+            new_data["curve"] = [self.parser.parse_expression_string_to_lambda(new_data["surface"][0]),
+                                 self.parser.parse_expression_string_to_lambda(new_data["surface"][1]),
+                                 self.parser.parse_expression_string_to_lambda(new_data["surface"][2])]
 
             self.objManager.update_paramteric_surface(uid, **new_data)
         else:
@@ -249,9 +249,9 @@ class ObjectStorage:
         elif to_create["FigureTypes"] == FigureTypes.PARAMETRIC_SURFACE:
             to_create["curve_string"] = to_create["curve"]
 
-            to_create["curve"] = [self.parser.parse_expression_string_to_lambda(to_create["curve"][0]),
-                                  self.parser.parse_expression_string_to_lambda(to_create["curve"][1]),
-                                  self.parser.parse_expression_string_to_lambda(to_create["curve"][2])]
+            to_create["curve"] = [self.parser.parse_expression_string_to_lambda(to_create["surface"][0]),
+                                  self.parser.parse_expression_string_to_lambda(to_create["surface"][1]),
+                                  self.parser.parse_expression_string_to_lambda(to_create["surface"][2])]
             uid = self.objManager.create_paramteric_surface(**to_create)
         else:
             raise Exception(f"Invalid Figure type {to_create['FigureTypes']}")
