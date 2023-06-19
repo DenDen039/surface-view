15d21a7082f969b9f72ffc5ea01ca34c35a7e355
Jeremy Kovalchuk <112754369+H3llAbyss@users.noreply.github.com>
2023-06-19 18:43:20 +0300

    PEP 8 and other fixes (#32)
    * Big bugfix
    All settingsare working
    Added new scene functional
    Added new icons
    
    * Added pep8 comments
    
    * Added pep8 comments
    
    * Added pep8 comments
    
    * Added screenshots error handling
    
    * Fixed some parsing

--- package/qt_widgets/plotter_widget.py
+++ package/qt_widgets/plotter_widget.py
@@ -2,7 +2,35 @@ from pyvistaqt import BackgroundPlotter
 from package.figures.figure import FigureTypes
 from PyQt5 import QtWidgets
 
+
 class PlotterWidget(QtWidgets.QWidget):
+    '''A widget used for creating and displaying scenes.
+
+       Args:
+           parent (QWidget, optional): Parent widget. Default is None.
+
+       Attributes:
+           plotter (BackgroundPlotter): Object for plotting figures.
+           layout (QVBoxLayout): Layout manager for placing the plotter widget.
+           meshes (dict): Dictionary for storing mesh data.
+           actors (dict): Dictionary for storing actor objects.
+           actors_types (dict): Dictionary for storing actor types.
+           actors_settings (dict): Dictionary for storing actor settings.
+           actors_HL (dict): Dictionary for storing highlighted actors.
+           actors_labels (dict): Dictionary for storing actor labels.
+           actors_drawed_labels (dict): Dictionary for storing drawn actor labels.
+           intersections_list (list): List of intersections.
+           photo_counter (int): Counter for untitled photos.
+           __highlight_color (str): Default highlight color.
+           __highlight_width (float): Default highlight width.
+           __label_colors (list): List of colors for labels.
+           __intersection_color (str): Default intersection color.
+           __intersection_width (float): Default intersection width.
+           __labels_enabled (bool): Flag indicating whether labels are enabled.
+           __label_width (int): Label width.
+           __point_size (int): Point size.
+           __font_size (int): Font size.
+       '''
     def __init__(self, parent=None):
         super().__init__(parent=parent)
 
@@ -81,7 +109,7 @@ class PlotterWidget(QtWidgets.QWidget):
     def font_size(self, value: float):
         self.__font_size = value
 
-    def add_mesh(self, uid: str, mesh, figure_type, labels, **kwargs):
+    def add_mesh(self, uid: str, mesh, figure_type, labels, **kwargs) -> None:
         if uid in self.actors:
             raise Exception("Figure already exists")
         self.meshes[uid] = mesh
@@ -91,19 +119,19 @@ class PlotterWidget(QtWidgets.QWidget):
         self.actors_labels[uid] = labels
         self.update_camera()
 
-    def hide_mesh(self, uid: str):
+    def hide_mesh(self, uid: str) -> None:
         if uid not in self.actors:
             raise Exception("Figure not exist")
         self.remove_mesh(uid)
         self.actors[uid] = self.plotter.add_mesh(self.meshes[uid], opacity=0)#, **self.actors_settings[uid])
 
-    def show_mesh(self, uid: str):
+    def show_mesh(self, uid: str) -> None:
         if uid not in self.actors:
             raise Exception("Figure not exist")
         self.remove_mesh(uid)
         self.actors[uid] = self.plotter.add_mesh(self.meshes[uid], **self.actors_settings[uid])
 
-    def remove_mesh(self, uid: str):
+    def remove_mesh(self, uid: str) -> None:
         if uid not in self.actors:
             raise Exception("Figure not exist")
         if uid in self.actors_HL:
@@ -112,7 +140,7 @@ class PlotterWidget(QtWidgets.QWidget):
         self.actors.pop(uid)
         self.update_camera()
 
-    def highlight_mesh(self, uid: str, color: str='red', line_width: float=2.5):
+    def highlight_mesh(self, uid: str, color: str='red', line_width: float=2.5) -> None:
 
         color = self.highlight_color
         if uid not in self.actors:
@@ -135,7 +163,7 @@ class PlotterWidget(QtWidgets.QWidget):
         else:
             raise Exception("Unknown figure type")
         
-    def remove_highlight(self, uid: str):
+    def remove_highlight(self, uid: str) -> None:
 
         if uid in self.actors_HL:
             if self.actors_types[uid] in [FigureTypes.CONE, FigureTypes.CYLINDER, FigureTypes.PLANE, FigureTypes.REVOLUTION, FigureTypes.PARAMETRIC_SURFACE]:
@@ -145,19 +173,19 @@ class PlotterWidget(QtWidgets.QWidget):
                   self.remove_mesh(uid)
                   self.actors[uid] = self.plotter.add_mesh(self.meshes[uid], **self.actors_settings[uid])
 
-    def show_edges_mesh(self, uid: str, color: str='white'):
+    def show_edges_mesh(self, uid: str, color: str='white') -> None:
         if uid not in self.actors:
             raise Exception("Figure not exist")
         self.remove_mesh(uid)
         self.actors[uid] = self.plotter.add_mesh(self.meshes[uid], show_edges=True, edge_color=color, **self.actors_settings[uid])
 
-    def hide_edges_mesh(self, uid: str):
+    def hide_edges_mesh(self, uid: str) -> None:
         if uid not in self.actors_HL:
             raise Exception("Figure not exist")
         self.remove_mesh(uid)
         self.actors[uid] = self.plotter.add_mesh(self.meshes[uid], show_edges=False, **self.actors_settings[uid])
 
-    def add_intersections(self, intersections, colors, line_width):
+    def add_intersections(self, intersections, colors, line_width) -> None:
         print(f"intersections:{intersections}")
 
         for i in range(len(intersections)):
@@ -165,13 +193,15 @@ class PlotterWidget(QtWidgets.QWidget):
                                                      line_width=line_width)
             self.intersections_list.append(new_intersection)
 
-    def remove_intersections(self):
+    def remove_intersections(self) -> None:
         for item in self.intersections_list:
             self.plotter.remove_actor(item)
         self.intersections_list.clear()
         self.update_camera()
 
-    def add_label(self, uid, point_size=14, line_width=8, font_size=12):
+
+    def add_label(self, uid, point_size=14, line_width=8, font_size=12) -> None:
+
         if not self.labels_enabled:
             return
         point_size = self.__point_size
@@ -190,60 +220,66 @@ class PlotterWidget(QtWidgets.QWidget):
         drawed_points = [self.plotter.actors[list(self.plotter.actors.keys())[-2]], self.plotter.actors[list(self.plotter.actors.keys())[-1]]]
         self.actors_drawed_labels[uid] = drawed_meshes + drawed_points
 
-    def remove_label(self, uid):
+    def remove_label(self, uid) -> None:
         if uid not in self.actors_drawed_labels.keys():
             return
         for actor in self.actors_drawed_labels[uid]:
             self.plotter.remove_actor(actor)
 
-    def clear_actors(self):
+    def clear_actors(self) -> None:
         for actor in list(self.actors.keys()):
             self.remove_mesh(actor)
         self.actors = dict()
         self.update_camera()
 
-    def unlock_camera(self):
+    def unlock_camera(self) -> None:
         self.fly = self.plotter.enable_fly_to_right_click()
 
-    def lock_camera(self):
+    def lock_camera(self) -> None:
         self.update_camera()
 
-    def update_camera(self):
+    def update_camera(self) -> None:
         self.plotter.view_isometric()
 
     def get_bounds(self) -> tuple:
         return self.plotter.bounds
         
-    def view_xy(self):
+    def view_xy(self) -> None:
         self.plotter.view_xy()
     
-    def view_xz(self):
+    def view_xz(self) -> None:
         self.plotter.view_xz()
 
-    def view_yx(self):
+    def view_yx(self) -> None:
         self.plotter.view_yx()
 
-    def view_yz(self):
+    def view_yz(self) -> None:
         self.plotter.view_yz()
     
-    def view_zx(self):
+    def view_zx(self) -> None:
         self.plotter.view_zx()
 
-    def view_zy(self):
+    def view_zy(self) -> None:
         self.plotter.view_zy()
 
-    def blur(self):
+    def blur(self) -> None:
         self.plotter.add_blurring()
     
-    def remove_blur(self):
+    def remove_blur(self) -> None:
         self.plotter.remove_blurring()
 
-    def take_screenshot(self, extension: str, file_path=''):
+    def take_screenshot(self, extension: str, file_path='') -> None:
         if file_path == '':
             file_name = 'untitled_' + str(self.photo_counter) + extension
-            self.photo_counter += 1
+            try:
+                self.photo_counter += 1
 
-            self.plotter.screenshot(f"photos/{file_name}")
+                self.plotter.screenshot(f"photos/{file_name}")
+            except PermissionError as e:
+                raise e
+            except Exception as e:
+                raise e
+
         else:
             if file_path.split('.')[-1] not in ['png', 'jpeg', 'jpg', 'bmp', 'tif', 'tiff']:
                 raise Exception("Unfortunately, this graphic format is not supported")
@@ -265,12 +301,3 @@ class PlotterWidget(QtWidgets.QWidget):
             return max(numbers) + 1
         else:
             return 0
-
-        # files = os.listdir("package/photos/")
-        #     numbers = list(filter(lambda str: str.startswith("untitled_"), files))
-        #     numbers = [int(numbers[i].split('untitled_')[-1].split('.png')[0]) for i in range(len(numbers))]
-        #     if numbers:
-        #         print(numbers)
-        #         return max(numbers) + 1
-        #     else:
-        #         return 0
